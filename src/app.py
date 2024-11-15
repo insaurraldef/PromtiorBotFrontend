7e7eda96e9bee0578f021ecc47416f8ecf4c5db8
finsuarralde <finsaurralde@bdsol.com.ar>
2024-11-15 13:50:43 -0300

    fixing errors

--- src/app.py
+++ src/app.py
@@ -6,20 +6,21 @@ from flask.cli import load_dotenv
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 load_dotenv()
-backend_url = os.getenv("BACKEND_URL")
+BACKEND_URL = os.getenv("BACKEND_URL")
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', backend_url=BACKEND_URL)
 
 @app.route('/ask', methods=['POST'])
 def ask():
-    question = request.form.get('input')
+    data = request.get_json()
+    question = data.get('input')
     try:
-        response = requests.post(f"{backend_url}/chatbot", json={"input": question})
+        response = requests.post(f"{BACKEND_URL}/chatbot", json={"input": question})
         return jsonify(response.json())
     except Exception as e:
         return jsonify({"response": f"Error: {str(e)}"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5002)
